6f60f24b5f364d0e4a5695971467c963ee4a5086
linbaozhong <lin.baozhong@hotmail.com>
2016-06-18 16:14:10 +0800

    2

---
+++ Common/DataCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace Snow
+{
+  public class DataCache
+  {
+
+    public static object GetCache(string CacheKey)
+    {
+      return HttpRuntime.Cache[CacheKey];
+    }
+
+    public static void SetCache(string CacheKey, object objObject)
+    {
+      HttpRuntime.Cache.Insert(CacheKey, objObject);
+    }
+
+    public static void SetCache(string CacheKey, object objObject, DateTime absoluteExpiration, TimeSpan slidingExpiration)
+    {
+      HttpRuntime.Cache.Insert(CacheKey, objObject, (CacheDependency) null, absoluteExpiration, slidingExpiration);
+    }
+
+    public static void DeleteCache(string CacheKey)
+    {
+      HttpRuntime.Cache.Remove(CacheKey);
+    }
+  }
+}
---
+++ Documents/readme.txt
@@ -0,0 +1,4 @@
+﻿Cache的结构：
+1、command String
+2、parameters  SqlParameter[]
+3、properties  PropertyInfo[]
